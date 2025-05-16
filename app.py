6ccd6ff0c7d6ca869f662cddbfaf3a7ef1ed66e5
WesleySephai <124874511+WesleySephai@users.noreply.github.com>
2025-05-16 17:58:03 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+@app.route('/')
+def index():
+    items = Item.query.all()
+    return render_template('index.html', items=items)
+
+@app.route('/add', methods=['POST'])
+def add():
+    name = request.form.get('name')
+    if name:
+        new_item = Item(name=name)
+        db.session.add(new_item)
+        db.session.commit()
+    return redirect('/')
+
+@app.route('/delete/<int:item_id>')
+def delete(item_id):
+    item = Item.query.get(item_id)
+    if item:
+        db.session.delete(item)
+        db.session.commit()
+    return redirect('/')
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
